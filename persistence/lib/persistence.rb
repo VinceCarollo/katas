b66a232e862cac1d285884695625395337ef42f1
Vince Carollo <vincecarollo@gmail.com>
2019-10-04 15:17:42 -0600

    Add kata description to persistence

--- persistence/lib/persistence.rb
+++ persistence/lib/persistence.rb
@@ -1,3 +1,8 @@
+# Write a function, persistence, that takes in a positive parameter num
+# and returns its multiplicative persistence, which is the number of times
+# you must multiply the digits in num until you reach a single digit.
+
+
 require 'pry'
 
 def persistence(num)
