42a75ce1625f95aba5f4314a939e7dc882d68ad4
Vince Carollo <vincecarollo@gmail.com>
2019-10-07 17:12:55 -0600

    Finish valid parentheses

--- valid_parentheses/lib/valid_parentheses.rb
+++ valid_parentheses/lib/valid_parentheses.rb
@@ -1,17 +1,11 @@
 require 'pry'
 
 def valid_parentheses(str)
-  return true if str == ''
-  pars = str.split('').select do |char|
-    ['(', ')'].include?(char)
+  open = 0
+  str.chars.each do |char|
+    open += 1 if char == '('
+    open -= 1 if char == ')'
+    return false if open < 0
   end
-  truthy = pars.map do |par|
-    if par == '(' && pars[pars.index(par)..-1].include?(')')
-      pars[pars.index(')')] = true
-      true
-    elsif par != true
-      false
-    end
-  end
-  truthy.uniq.length == 1 && truthy.uniq.first == true
+  open == 0
 end
--- valid_parentheses/spec/valid_parentheses_spec.rb
+++ valid_parentheses/spec/valid_parentheses_spec.rb
@@ -2,11 +2,11 @@ require './lib/valid_parentheses'
 
 RSpec.describe 'Valid Parentheses' do
   it "can tell if string has valid parentheses open and close" do
-    # expect(valid_parentheses("  (")).to be false
-    # expect(valid_parentheses(")test")).to be false
-    # expect(valid_parentheses("")).to be true
-    # expect(valid_parentheses("hi())(")).to be false
+    expect(valid_parentheses("  (")).to be false
+    expect(valid_parentheses(")test")).to be false
+    expect(valid_parentheses("")).to be true
+    expect(valid_parentheses("hi())(")).to be false
     expect(valid_parentheses("hi(hi)()")).to be true
-    # expect(valid_parentheses("())(()")).to be false
+    expect(valid_parentheses("())(()")).to be false
   end
 end
